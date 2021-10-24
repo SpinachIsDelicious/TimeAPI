cfa65c486786a055993c74a8abe4815d75f9a987
SpinachIsDelicious <91394707+SpinachIsDelicious@users.noreply.github.com>
2021-10-24 14:08:28 -0700

    Removed unnecessary lines of code
    Removed unnecessary lines of code

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ response = requests.get("http://worldtimeapi.org/api/ip")
 
 json_data = json.loads(response.text)
 
-# pprint.pprint(json_data)
-
 for value in json_data:
      printValue = str(value).title().replace('_', ' ')
      print(printValue+": "+str(json_data[value]))
